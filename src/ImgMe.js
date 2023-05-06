81543af5562d0584ec0d2891ac4e6bd916035fbc
theerakorn <theerakon.dev@gmail.com>
2023-05-07 02:38:46 +0700

    First-Folio-Me

---
+++ src/ImgMe.js
@@ -0,0 +1,8 @@
+function ImgMee() {
+    return (
+        <div className="con-img">
+            <img src={process.env.PUBLIC_URL + '/chas.jpg'} alt="chas"></img>
+        </div>
+    )
+}
+export default ImgMee
---
+++ src/section.js
@@ -0,0 +1,57 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import { faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab); // เพิ่ม package ของ free-brands-svg-icons เข้าไปใน library ของ FontAwesome
+
+function All() {
+    return (
+        <div className="title">
+                <div className="con-text">
+                    <h1>Hi, I'm <span>Theerakorn</span></h1>
+                    <h2>I'm a Font-End Developer.</h2>
+                </div>
+                <div className="con-contact">
+                    <p><FontAwesomeIcon icon={faLocationDot} className="address" /> Songkhla, Thailand</p>
+                    <p><FontAwesomeIcon icon={faEnvelope} className="email"  /> theerakorn.dev@gmail.com</p>
+                </div>
+                <div className="con-skills">
+                    <h4>PROGRAMMIMG / FRAMWORK</h4>
+                    <div className="card-skills">
+                        <div className="skill">
+                            <FontAwesomeIcon icon={['fab', 'html5']} style={{ color: '#ee652c' }} />
+                        </div>
+                        <div className="skill">
+                            <FontAwesomeIcon icon={['fab', 'css3-alt']} style={{ color: '#20abdb' }} />
+                        </div>
+                        <div className="skill">
+                            <FontAwesomeIcon icon={['fab', 'js']} style={{ color: '#f9d837' }} />
+                        </div>
+                        <div className="skill">
+                            <FontAwesomeIcon icon={['fab', 'php']} style={{ color: '#787cb4' }} />
+                        </div>
+                        <div className="skill">
+                            <FontAwesomeIcon icon={['fab', 'react']} style={{ color: '#00d8fe' }} />
+                        </div>
+                    </div>
+                </div>
+                <div className="con-tools">
+                    <h4>TOOLS</h4>
+                    <div className="card-tools">
+                        <div className="tool">
+                            <FontAwesomeIcon icon={['fab', 'git-alt']} style={{ color: '#f05133' }} />
+                        </div>
+                        <div className="tool">
+                            <FontAwesomeIcon icon={['fab', 'github']} style={{ color: '#171516' }} />
+                        </div>
+                        <div className="tool">
+                            <img src={process.env.PUBLIC_URL + 'vscode.png'}></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+export default All
